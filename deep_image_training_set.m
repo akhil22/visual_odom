ce8395a546e39151f1095af70dd8a440b26328ce
akhil Nagariya <akhil.nagariya@gmail.com>
2015-10-17 15:46:08 +0530

    add more comments

--- deep_image_training_set.m
+++ deep_image_training_set.m
@@ -1,7 +1,7 @@
 clear all;close all;
-list = dir('/home/akhil/Desktop/visual_odom/seq_0/Left/data');
+list = dir('/home/akhil/visual_odom/seq_0/Right/data');
 for i = 3:length(list)
-    I = (imread(strcat('/home/akhil/Desktop/visual_odom/seq_0/Left/data/',list(i).name)));
+    I = (imread(strcat('/home/akhil/visual_odom/seq_0/Right/data/',list(i).name)));
 %     imshow(I);
     xi = [499.5446;649.1831;761.4120;257.1301;474.1060;499.5446];
     yi = [235.1361;233.6398;371.3072;372.8036;233.6398;235.1361];
@@ -9,5 +9,5 @@ for i = 3:length(list)
 
     bw = roipoly(I,xi,yi);
 %      I(~bw) = 0;
-    imwrite(I,strcat('/home/akhil/Desktop/visual_odom/seq_0/left_fast_match/',int2str(i),'.jpg'),'jpg');
+    imwrite(I,strcat('/home/akhil/visual_odom/seq_0/right_rijvi_match/',int2str(i),'.jpg'),'jpg');
 end
